3335bdd83e52f3ad702b9578889b39ed85e0a84f
Boxfox <boxfoxsg619@gmail.com>
2018-05-27 19:31:45 +0900

    Implement wallet lookup router

--- src/main/java/com/boxfox/core/router/WalletRouter.java
+++ src/main/java/com/boxfox/core/router/WalletRouter.java
@@ -6,12 +6,44 @@ import com.boxfox.support.vertx.router.RouteRegistration;
 import com.boxfox.support.vertx.router.Param;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 
+import static com.boxfox.support.data.PostgresConfig.createContext;
+import static io.one.sys.db.Tables.COIN;
+import static io.one.sys.db.tables.Wallet.WALLET;
+
 public class WalletRouter {
 
-    @RouteRegistration(uri = "/wallet:name/balance", method = HttpMethod.GET, auth = true)
+    @RouteRegistration(uri = "/support/wallet/", method = HttpMethod.GET, auth = true)
+    public void getSupportWalletList(RoutingContext ctx){
+        JsonArray coins = new JsonArray();
+        createContext().selectFrom(COIN).fetch().forEach(r-> coins.add(new JsonObject(r.formatJSON())));
+        ctx.response().end(coins.encode());
+    }
+
+    @RouteRegistration(uri = "/wallet/", method = HttpMethod.GET, auth = true)
+    public void getWallets(RoutingContext ctx) {
+        String ownUid = (String) ctx.data().get("uid");
+        JsonArray wallets = new JsonArray();
+        createContext()
+                .select()
+                .from(WALLET.join(COIN).on(WALLET.SYMBOL.eq(COIN.SYMBOL)))
+                .where(WALLET.UID.equal(ownUid))
+                .fetch()
+                .forEach(r->{
+                    String symbol = (String) r.getValue("symbol");
+                    String address = (String) r.getValue("address");
+                    String balance = WalletServiceManager.getService(symbol).getBalance(address);
+                    JsonObject obj = new JsonObject(r.formatJSON());
+                    obj.put("balance", balance);
+                    wallets.add(obj);
+                });
+        ctx.response().end(wallets.encode());
+    }
+
+    @RouteRegistration(uri = "/wallet/:name/balance", method = HttpMethod.GET, auth = true)
     public void getBalance(RoutingContext ctx, @Param String address) {
         String name = ctx.pathParam("name");
         WalletService service = WalletServiceManager.getService(name);
