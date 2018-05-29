68f3ba410b1c15300ddf281ae0ffae750710d9fd
Boxfox <boxfoxsg619@gmail.com>
2018-05-29 20:14:25 +0900

    Implement the transaction indexing logic

--- src/main/java/com/boxfox/service/wallet/EthereumService.java
+++ src/main/java/com/boxfox/service/wallet/EthereumService.java
@@ -26,6 +26,8 @@ import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.EthBlock.Block;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.protocol.core.methods.response.Transaction;
@@ -33,7 +35,6 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.utils.Convert;
 import org.web3j.utils.Numeric;
-import scala.math.BigInt;
 
 public class EthereumService extends WalletService {
 
@@ -168,6 +169,41 @@ public class EthereumService extends WalletService {
   }
 
   private void indexingTransactions(String address){
+    try {
+      BigInteger lastBlockNumber = web3.ethBlockNumber().send().getBlockNumber();
+      int txCount = web3.ethGetTransactionCount(address, DefaultBlockParameterName.LATEST).send().getTransactionCount().intValue();
+      int balance = web3.ethGetBalance(address, DefaultBlockParameterName.LATEST).send().getBalance().intValue();
+      int idx = lastBlockNumber.intValue();
+      for (; idx >= 0 && (txCount > 0 || balance > 0); --idx) {
+        Block block = web3.ethGetBlockByNumber(DefaultBlockParameter.valueOf(BigInteger.valueOf(idx)), false).send().getBlock();
+        List<EthBlock.TransactionResult> transactions =  block.getTransactions();
+        if (block!=null && block.getTransactions()!=null) {
+          for(EthBlock.TransactionResult txResult : transactions){
+            Transaction tx = (Transaction) txResult.get();
+            if (address.equals(tx.getFrom())) {
+              if (!tx.getFrom().equals(tx.getTo()))
+                balance += tx.getValue().intValue();
+              txCount-=1;
+              indexingTransaction(tx);
+            }
+            if (address.equals(tx.getTo())) {
+              if (!tx.getFrom().equals(tx.getTo()))
+                balance-= tx.getValue().intValue();
+              indexingTransaction(tx);
+            }
+          }
+        }
+      }
+    } catch (IOException e) {
 
+    }
+  }
+
+  private void indexingTransaction(Transaction tx) {
+    String from = tx.getFrom();
+    String to = tx.getTo();
+    String txHash = tx.getHash();
+    BigInteger amount = tx.getValue();
+    createContext().insertInto(TRANSACTION).values(from, to, amount, txHash);
   }
 }
