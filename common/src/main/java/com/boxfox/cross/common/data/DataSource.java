8632e12dbcd11b56cc5f9c93234c4a84820c715a
Boxfox <boxfoxsg619@gmail.com>
2018-06-01 17:31:15 +0900

    Modify database connection setting

--- common/src/main/java/com/boxfox/cross/common/data/DataSource.java
+++ common/src/main/java/com/boxfox/cross/common/data/DataSource.java
@@ -21,7 +21,7 @@ public class DataSource {
         private static String user = Config.getDefaultInstance().getString("dbUser", "postgres");
         private static String password = Config.getDefaultInstance().getString("dbPassword", "dbpw1234");
         private static int port = Config.getDefaultInstance().getInt("dbPort", 5432);
-        private static String dbName = Config.getDefaultInstance().getString("crossdb");
+        private static String dbName = Config.getDefaultInstance().getString("dbName", "crossdb");
         private static DataSource instance = new DataSource(host, port, dbName, user, password);
     }
 
