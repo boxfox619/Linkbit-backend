244ddcf2b1f1b1e276344f8b83dd14765c3c3ecd
Boxfox <boxfoxsg619@gmail.com>
2018-06-02 02:03:34 +0900

    Modify the supported wallet list

--- src/main/java/com/boxfox/core/router/wallet/WalletRouter.java
+++ src/main/java/com/boxfox/core/router/wallet/WalletRouter.java
@@ -1,14 +1,19 @@
 package com.boxfox.core.router.wallet;
 
+import com.boxfox.cross.common.data.PostgresConfig;
 import com.boxfox.cross.service.AddressService;
 import com.boxfox.cross.common.vertx.router.RouteRegistration;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import io.one.sys.db.tables.daos.CoinDao;
+import io.one.sys.db.tables.pojos.Coin;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import org.jooq.DSLContext;
+
+import java.util.List;
 
 import static com.boxfox.cross.common.data.PostgresConfig.createContext;
 import static io.one.sys.db.Tables.COIN;
@@ -27,8 +32,11 @@ public class WalletRouter {
 
     @RouteRegistration(uri = "/support/wallet/list", method = HttpMethod.GET, auth = true)
     public void getSupportWalletList(RoutingContext ctx) {
+        CoinDao dao = new CoinDao(PostgresConfig.create());
         JsonArray coins = new JsonArray();
-        createContext().selectFrom(COIN).fetch().forEach(r -> coins.add(new JsonObject(r.formatJSON())));
+        dao.findAll().forEach(c -> {
+            coins.add(new JsonObject().put("symbol", c.getSymbol()).put("name", c.getName()));
+        });
         ctx.response().end(coins.encode());
     }
 }
