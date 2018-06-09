f08cd3bbdf5246e01f325b775e0821c195092017
Boxfox <boxfoxsg619@gmail.com>
2018-06-09 23:56:03 +0900

    Refactoring friend router

--- src/main/java/com/boxfox/core/router/FriendRouter.java
+++ src/main/java/com/boxfox/core/router/FriendRouter.java
@@ -1,17 +1,13 @@
 package com.boxfox.core.router;
 
-import com.boxfox.cross.common.data.PostgresConfig;
 import com.boxfox.cross.common.vertx.router.Param;
 import com.boxfox.cross.common.vertx.router.RouteRegistration;
 import com.boxfox.cross.common.vertx.service.Service;
 import com.boxfox.cross.service.FriendService;
-import com.boxfox.cross.service.model.Profile;
 import com.google.gson.Gson;
-import io.one.sys.db.tables.daos.AccountDao;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
-import static io.one.sys.db.Tables.FRIEND;
 
 public class FriendRouter {
 
@@ -19,11 +15,11 @@ public class FriendRouter {
     private FriendService friendService;
     private Gson gson;
 
-    public FriendRouter(){
+    public FriendRouter() {
         this.gson = new Gson();
     }
 
-    @RouteRegistration(uri = "/search/account/:type", method = HttpMethod.GET, auth = true)
+    @RouteRegistration(uri = "/search/user", method = HttpMethod.GET, auth = true)
     public void search(RoutingContext ctx, @Param String type, @Param String text) {
         friendService.serachUsers(type, text, res -> {
             ctx.response().end(new Gson().toJson(res.result()));
@@ -33,32 +29,31 @@ public class FriendRouter {
     @RouteRegistration(uri = "/friend", method = HttpMethod.GET, auth = true)
     public void loadFriends(RoutingContext ctx) {
         String uid = (String) ctx.data().get("uid");
-        friendService.loadFriends(uid, res->{
+        friendService.loadFriends(uid, res -> {
             ctx.response().end(gson.toJson(res.result()));
         });
     }
 
 
     @RouteRegistration(uri = "/friend", method = HttpMethod.PUT, auth = true)
-    public void addFriend(RoutingContext ctx, @Param String uid) {
+    public void addFriend(RoutingContext ctx, @Param String targetUid) {
         String ownUid = (String) ctx.data().get("uid");
-        friendService.addFriend(ownUid, uid, res->{
-            if(res.succeeded()){
+        friendService.addFriend(ownUid, targetUid, res -> {
+            if (res.succeeded()) {
                 ctx.response().setStatusCode(200).end();
-            }else{
+            } else {
                 ctx.fail(res.cause());
             }
         });
     }
 
-
     @RouteRegistration(uri = "/friend", method = HttpMethod.DELETE, auth = true)
     public void deleteFriend(RoutingContext ctx, @Param String uid) {
         String ownUid = (String) ctx.data().get("uid");
         friendService.deleteFriend(ownUid, uid, res -> {
-            if(res.succeeded()){
+            if (res.succeeded()) {
                 ctx.response().setStatusCode(200).end();
-            }else{
+            } else {
                 ctx.fail(res.cause());
             }
         });
