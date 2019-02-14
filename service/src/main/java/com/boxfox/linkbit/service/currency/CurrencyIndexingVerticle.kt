ad5153c9d8cc07b55b335caef17e5bfb4ba1d8f2
Boxfox <boxfoxsg619@gmail.com>
2019-02-15 00:36:05 +0900

    Fix currency indexing logic

--- service/src/main/java/com/boxfox/linkbit/service/currency/CurrencyIndexingVerticle.kt
+++ service/src/main/java/com/boxfox/linkbit/service/currency/CurrencyIndexingVerticle.kt
@@ -17,20 +17,27 @@ class CurrencyIndexingVerticle(private val logger: Logger = LogManager.getRootLo
                                private val localeDao: LocaleDao = LocaleDao(PostgresConfig.create())) : AbstractVerticle() {
     private val CURRENCY_API = "https://free.currencyconverterapi.com/api/v6/convert?apiKey=1b68d72a5f4db84bf356&q=%s&compact=ultra"
     override fun start() {
+        indexing()
         currencyParsingCycle()
     }
 
-    private fun currencyParsingCycle() {
-        CompletableFuture.delayedExecutor(60, TimeUnit.MINUTES).execute {
-            logger.debug("indexing currency")
-            localeDao.findAll().filter { !it.currency.equals("USD") }.forEach{
-                val currency = it.currency.toUpperCase()
-                val key= String.format("USD_%s", currency)
-                val money = Unirest.get(String.format(CURRENCY_API, key )).asJson().body.`object`.getDouble(key)
-                jedisPool.resource.use { jedis ->
-                    jedis.hset("Money", currency, money.toString())
-                }
+    private fun indexing() {
+        logger.debug("indexing currency")
+        localeDao.findAll().filter { !it.currency.equals("USD") }.forEach{
+            val currency = it.currency.toUpperCase()
+            val key= String.format("USD_%s", currency)
+            logger.debug(String.format("indexing currency %s", key))
+            val money = Unirest.get(String.format(CURRENCY_API, key )).asJson().body.`object`.getDouble(key)
+            jedisPool.resource.use { jedis ->
+                jedis.hset("Money", currency, money.toString())
             }
+        }
+    }
+
+    private fun currencyParsingCycle() {
+        CompletableFuture.delayedExecutor(60, TimeUnit.HOURS).execute {
+            indexing()
+            currencyParsingCycle()
         }
     }
 }
